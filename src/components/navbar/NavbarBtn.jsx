a5c9e38f2c55be93a237818166932a320fd87f98
ROSHAN KUMAR SINHA <roshankumarsinha@ROSHANs-MacBook-Pro.local>
2025-06-09 22:43:23 +0530

    Fix img path

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import { FaFileDownload } from "react-icons/fa";
 const NavbarBtn = () => {
   return (
     <button className="px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
-      <a href="../../public/resume/resume.pdf" download>
+      <a href="/resume/resume.pdf" download>
         Resume
       </a>
       <div className="sm:hidden md:block">
--- src/components/toolsSection/SubTools.jsx
+++ src/components/toolsSection/SubTools.jsx
@@ -3,7 +3,7 @@ const SubTools = () => {
     <div className="border-y-2 border-lightGrey relative">
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
       <img
-        src="../../public/images/subTools.png"
+        src="/images/subTools.png"
         alt="subTools image"
         className="lg:h-[104px] lg:w-[1290px] mx-auto md:h-[51px] md:w-[600px] sm:h-[25px] sm:w-[300px]"
         loading="lazy"
